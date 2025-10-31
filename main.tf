fd29df7e191f38fe8441100646195c02d3dd4d12
GyaneshBamrara <112647673+GyaneshBamrara@users.noreply.github.com>
2025-10-31 19:54:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -110,8 +110,7 @@ resource "aws_s3_bucket_public_access_block" "bg_s3_prac_block" {
 
   block_public_acls       = true
   block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  true
 }
 
 # EC2 Instance
